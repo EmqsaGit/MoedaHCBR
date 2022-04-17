ea2706ed8dff609b4fdc096f89df120d86099d90
EmqsaGit <1Oliveira#>
2022-04-16 22:49:02 -0300

    Added more currency

--- src/main/java/net/emqsagit/moedahcbr/item/ModItems.java
+++ src/main/java/net/emqsagit/moedahcbr/item/ModItems.java
@@ -11,7 +11,20 @@ public class ModItems {
     public static final Item UM_CENTAVO = registerItem("um_centavo",
             new Item(new FabricItemSettings().group(ItemGroup.MISC)));
 
+    public static final Item DEZ_CENTAVOS = registerItem("dez_centavos",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
 
+    public static final Item VC_CENTAVOS = registerItem("vinte_e_cinco_centavos",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item CQ_CENTAVOS = registerItem("cinquenta_centavo",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item UM_HERMITDOLLAR = registerItem("um_hermit_dollar",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item DEZ_HERMITDOLLARS = registerItem("dez_hermit_dollars",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(MoedaHcbr.MOD_ID, name), item);
